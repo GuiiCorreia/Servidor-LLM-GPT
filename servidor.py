d3f4147643b0ef0fcadc123f1a4881dd2ff0eb84
Guilherme Correia <guilhermecorreiadutra@gmail.com>
2024-05-30 00:29:48 -0300

    Reaproveitar um ID de  Assistant já criado, Pasta exclusiva para promtp

--- servidor.py
+++ servidor.py
@@ -14,32 +14,32 @@ CORS(app)
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 # Armazenar o ID do assistente globalmente
-assistant_id = None
+#assistant_id = None
 lock = threading.Lock()
 
 def initialize_assistant():
     global assistant_id
+    assistant_id_file = 'assistant_id.txt'
     with lock:
-        if assistant_id is None:
+        if os.path.exists(assistant_id_file):
+            with open(assistant_id_file, 'r', encoding='utf-8') as file:
+                assistant_id = file.read().strip()
+                print(f"Using existing assistant with ID: {assistant_id}")
+                
+        elif assistant_id is not None:
             print("Creating assistant...")
+            with open('prompt.txt', 'r', encoding='utf-8') as file:
+                prompt = file.read()
             my_assistant = client.beta.assistants.create(
                 model="gpt-3.5-turbo",
-                instructions="""Você é um assistente especializado em extrair o contexto das perguntas relacionadas ao processo Order to Cash (O2C). Quando o usuário enviar uma frase ou pergunta, você deve retornar apenas o contexto relevante encontrado na frase. O contexto deve ser apenas as palavras que capture o contexto de pergunta do usuário. Se você não souber, responda “Não sei”.
-Exemplo:
-    • Pergunta do usuário: "Qual é o número total de itens das ordens de vendas de em abril de 2023?"
-    • Contexto extraído: Numero total / Item das ordens de vendas / Abril 2023
-    • Pergunta do usuário: "Quais são os métodos de pagamento aceitos para a fatura?"
-    • Contexto extraído: Métodos de pagamento aceitos / Fatura.
-    • Pergunta do usuário: "O que devo fazer se houver um erro na minha fatura?"
-    • Contexto extraído: O que fazer / Erro na fatura.
-    • Pergunta do usuário: "Como posso resolver uma disputa de crédito?"
-    • Contexto extraído: Resolver / disputa de crédito.
-Use esses exemplos como guia para extrair o contexto das frases que os usuários enviarem. Retorne apenas o contexto relevante em uma frase curta. """,
                 name="Assistant O2C - Extrair Contexto",
+                instructions=prompt,
                 temperature=0.1,
                 #tools=[{"type":"file_search"}],
             )
             assistant_id = my_assistant.id
+            with open(assistant_id_file, 'w', encoding='utf-8') as file:
+                file.write(assistant_id)
             print(f"Assistant created with ID: {assistant_id}")
         else:
             print("Assistant already created with ID:", assistant_id)
